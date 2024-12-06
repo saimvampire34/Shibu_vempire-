b4047d90843dd755e2439fc52636a47c94089492
Radhe0777 <princek8356@gmail.com>
2024-12-06 10:56:35 +0530

    Update userbot.py

--- SONALI/core/userbot.py
+++ SONALI/core/userbot.py
@@ -14,7 +14,7 @@ class Userbot(Client):
             name="RAUSHANAss1",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
-            session_string=str(config.STRING1),
+            session_string=str(config.STRING1), 
             no_updates=True,
         )
         self.two = Client(
